013fd06d83fa1e59505616c1b926111d170801c0
taldigi <lucas7602@gmail.com>
2023-02-05 15:13:18 +0000

    mui work ok

--- src/Demo.js
+++ src/Demo.js
@@ -2,32 +2,37 @@ import React, { Component } from "react";
 import Chessboard from "chessboardjsx";
 
 import StockFish from "./integrations/Stockfish.js";
-
+import Button from "@mui/material/Button/Button.js";
 class Demo extends Component {
   render() {
     return (
-      <div style={boardsContainer}>
-        <StockFish>
-          {({ position, onDrop }) => (
-            <Chessboard
-              id="stockfish"
-              // FEN IS ADDED BUT NOT WORKING
-              //fen="rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBqKBNR w KQkq - 0 2"
-              /*     position={
+      <div>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+        <div style={boardsContainer}>
+          <StockFish>
+            {({ position, onDrop }) => (
+              <Chessboard
+                id="stockfish"
+                // FEN IS ADDED BUT NOT WORKING
+                //fen="rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBqKBNR w KQkq - 0 2"
+                /*     position={
                 position ==
                 "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
                   ? "rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBqKBNR w KQkq - 0 2"
                   : position
               }
               {...console.log(position)}*/
-              position={position}
-              width={700}
-              onDrop={onDrop}
-              boardStyle={boardStyle}
-              orientation="white"
-            />
-          )}
-        </StockFish>
+                position={position}
+                width={700}
+                onDrop={onDrop}
+                boardStyle={boardStyle}
+                orientation="white"
+              />
+            )}
+          </StockFish>
+        </div>
       </div>
     );
   }
