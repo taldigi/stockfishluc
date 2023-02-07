f5e66143007d70b578285b0cb33452abc71e3b1a
taldigi <lucas7602@gmail.com>
2023-02-07 17:18:15 +0000

    responsive in work

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import { Routes, Route } from "react-router-dom";
 
 import Puzzle from "../pages/Puzzle";
 import { IconButton } from "@mui/material";
+function menuHamburger() {
+  console.log("menuHamburger");
+  //change the class of the left-sidebar
 
+  //change the class of the burger
+
+  console.log();
+  let menuItems = "active";
+  if (document.querySelector(".left-sidebar").classList.toggle("active")) {
+    menuItems = "menu-active";
+    console.log(document.querySelector(".burger").classList.toggle("active"));
+    console.log(menuItems);
+  } else {
+    menuItems = "menu-inactive";
+    console.log(document.querySelector(".burger").classList.toggle("active"));
+    console.log(menuItems);
+  }
+}
 function Sidebar() {
+  let menuItems = "menu-inactive";
   return (
-    <div className="left-sidebar">
-      <i>
+    <div className={"left-sidebar" + menuItems}>
+      <i className="burger" onClick={menuHamburger}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="icon icon-tabler icon-tabler-menu-2"
