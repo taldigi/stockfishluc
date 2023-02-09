7eec8a50fe83d129540b178b35bbf3cfe74d5d3d
taldigi <lucas7602@gmail.com>
2023-02-09 12:38:15 +0000

    media qeris succefull

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, Routes, Route } from "react-router-dom";
 import Puzzle from "./pages/Puzzle";
-import SideNav from "./components/Sidebarmovile";
+import SideNav from "./components/SideNav";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Sidebarmovile from "./components/Sidebarmovile";
@@ -18,7 +18,7 @@ const App = () => {
   }, []);
   return (
     <div className="bodyClass">
-      <Media query="(max-width: 60px)">
+      <Media query="(max-width: 900px)">
         {(matches) => {
           if (matches) {
             return <Sidebarmovile />;
